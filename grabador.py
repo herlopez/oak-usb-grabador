7dcf8ce2557a5f752454f6c815c3462c331dd00f
Hernan Lopez <herlopezm@gmail.com>
2025-04-16 00:57:56 -0400

    Fix

--- grabador.py
+++ grabador.py
@@ -24,8 +24,8 @@ with dai.Device(pipeline) as device:
     # Configuración del grabador de video
     frame_width = 1920  # Ancho del frame (1080p)
     frame_height = 1080  # Alto del frame (1080p)
-    fps = 30  # FPS de la cámara
-    segment_duration = 60  # Duración del video en segundos (1 minuto)
+    fps = 10  # FPS de la cámara
+    segment_duration = 15  # Duración del video en segundos (1 minuto)
 
     # Crear el archivo de video
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
@@ -33,23 +33,25 @@ with dai.Device(pipeline) as device:
 
     start_time = time.time()
 
-    while True:
-        # Captura el frame
-        frame = q.get().getCvFrame()
+    try:
+        while True:
+            # Captura el frame
+            frame = q.get().getCvFrame()
 
-        # Escribir el frame en el archivo de video
-        out.write(frame)
+            # Escribir el frame en el archivo de video
+            out.write(frame)
 
-        # Mostrar el frame (opcional)
-        # cv2.imshow("frame", frame)
-        # if cv2.waitKey(1) == ord('q'):
-        #     break
+            # Mostrar el frame (opcional)
+            # cv2.imshow("frame", frame)
+            # if cv2.waitKey(1) == ord('q'):
+            #     break
 
-        # Verificar si ha pasado 1 minuto
-        if time.time() - start_time >= segment_duration:
-            print("Grabación de 1 minuto completada.")
-            break
-
-    # Liberar recursos
-    out.release()
-    cv2.destroyAllWindows()
+            # Verificar si ha pasado 1 minuto
+            if time.time() - start_time >= segment_duration:
+                print("Grabación de 1 minuto completada.")
+                break
+    finally:
+        # Liberar recursos
+        out.release()
+        cv2.destroyAllWindows()
+        print("Recursos liberados correctamente.")
